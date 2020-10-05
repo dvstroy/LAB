64bb9bbb4e2109cc142f5dc519dbdbaa50dc0eef
Connor <connorhayman99@gmail.com>
2020-10-05 22:34:06 +1300

    For Real This Time
    Video Games!! Missing the error shit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool DockKey, LivingKey, OfficeKey, ObserveKey;
+    public GameObject DockKeyObject, LivingKeyObject, OfficeKeyObject, ObserveKeyObject;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        DockKey = false;
+        LivingKey = false;
+        OfficeKey = false;
+        ObserveKey = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/PlayerController/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class CameraController : MonoBehaviour
+{
+   public enum ADSType
+    {
+        HoldToAim,
+        ClickToAim
+    }
+    // Start is called before the first frame update
+    [Header("Camera target position")]
+    public Transform Target;
+
+    [Header("Camera Aim Settings")]
+    public float VerticalSensitivity;
+    public float HorizontalSensitivity;
+    public float MaxLookUpAngle;
+    public float MinLookUpAngle;
+    public float FOV;
+    [Header("ADS")]
+    public float ADSspeed;
+    Vector2 mouseInput;
+    float rotX=0, rotY=0;
+    bool aimingDownSights = false;
+    float Xsensitivity;
+    float Ysensitivity;
+    Camera main;
+    public PlayerInputController playerInputController;
+    
+
+    void Start()
+    {
+        main = GetComponent<Camera>();
+       
+    }
+
+    private void Update()
+    {
+        #region CameraMovement
+        mouseInput = playerInputController.inputActions.Player.Look.ReadValue<Vector2>();
+        rotX += mouseInput.x * Xsensitivity;
+        rotY += mouseInput.y * Ysensitivity;
+        rotY = Mathf.Clamp(rotY, MinLookUpAngle, MaxLookUpAngle);
+        transform.localRotation = Quaternion.Euler(-rotY, rotX, 0f);
+        main.fieldOfView = Mathf.Lerp(main.fieldOfView, FOV, ADSspeed);
+        Xsensitivity = HorizontalSensitivity;
+        Ysensitivity = VerticalSensitivity;
+        #endregion
+
+
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Target.position;
+    }
+}
